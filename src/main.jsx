ff2bd8f4198a3a716deb202234ad27dc0e0b6ae0
softwaredeveloper111 <girisourav702939@gmail.com>
2026-06-19 19:08:11 +0530

    build(setup)!:setup folder structure and other things

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from "react-router-dom"
+import { Toaster } from "sonner"
 
-createRoot(document.getElementById('root')).render( <App />)
+
+createRoot(document.getElementById('root')).render(
+
+  <BrowserRouter>
+    <Toaster />
+    <App />
+  </BrowserRouter>
+
+)
